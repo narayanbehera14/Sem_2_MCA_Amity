169f1840eaa2ed7a30f82043db0c7bf09cf53f82
narayanbehera14 <beheranarayancs222330@gmail.com>
2026-02-25 09:53:00 +0530

    updated

---
+++ C++_LAB/prac2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Enter value of a: " << endl;
+    cin >> a;
+    cout << "a = " << a << endl;
+
+    int b;
+    cout << "Enter value of b: " << endl;
+    cin >> b;
+    cout << "b = " << b << endl;
+
+    int c;
+    c = a + b;
+    cout << "Sum = " << c << endl;
+
+    return 0;
+}
